6fd06dc2c9cf930180b560ca1f222cd75f298971
Vladyslav <vkadyrov@protonmail.com>
2022-11-26 17:06:36 -0500

    commit tree bfe557f67e318434033d8b4faf5e1f120e59d48f

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 function Home(props) {
   return (
-    <div id="home">
+    <div id='home'>
       <h1 style={{ color: props.color }}>
         {props.name} is a Web Developer from {props.city}
       </h1>
